847234391361aa1c68aab9a9b7391aad1a9a6f84
jithsg <135303624+jithsg@users.noreply.github.com>
2023-08-05 06:42:20 +0000

    third commit

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 install:
 	pip install --upgrade pip &&\
 		pip install -r requirements.txt
-lint:
-	pylint --disable=R,C *.py
-format:
-	black *.py
 test:
 	python -m pytest -vv test_hello.py
-all: install lint format test
+format:
+	black *.py
+lint:
+	pylint --disable=R,C *.py
+all: install lint test
--- myStatements.py
+++ myStatements.py
@@ -1,24 +1,18 @@
-"""## Learning Statements"""
+
 
 print("Hello world")
 
-"""### Variables"""
+
 
 variable = 2
 
-"""### List"""
 
 my_list = [1, 2, 3]
 
-"""### Dictionary"""
 
 my_dict = {1: "one", 2: "two"}
 
-my_dict[1]
-
 type(my_dict)
-
-"""### Loop"""
 
 for key, value in my_dict.items():
     print(f"This is my key : {key}")
